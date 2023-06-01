e13e852ef0d4c0c7b28861e8678b04d14cace087
Sudesha-STR <sudeshaiitm@gmail.com>
2023-06-01 08:27:46 +0530

    created delete option

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
@@ -10,7 +10,10 @@ from .forms import RoomForm
 
 def home(request):
     rooms = Room.objects.all()
-    context = {'rooms' :rooms}
+
+    topics = Topic.objects.all()
+
+    context = {'rooms' :rooms, 'topics':topics}
     return render(request, 'base/home.html',context)
 
 def room(request, pk):
@@ -34,5 +37,20 @@ def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
 
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    
+    return render(request, 'base/delete.html', {'obj':room})
